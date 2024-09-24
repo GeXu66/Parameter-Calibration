3113e31b6dcf2ad1edf93f3e3afed79c794eabfa
GeXu66 <2945642192@qq.com>
2024-09-24 20:57:47 +0800

    增加了绘图代码的数据截取

--- exp_plot.py
+++ exp_plot.py
@@ -25,28 +25,29 @@ if __name__ == '__main__':
         # 将数据添加到总的DataFrame中
         all_data = pd.concat([all_data, data], ignore_index=True)
     # 绘制图表
-    plt.figure(figsize=(10, 6))  # 设置图表大小
+    plt.figure()  # 设置图表大小
     # 计算RMSE
     rmse_values = []
     # 遍历每个唯一的实验编号，绘制每个实验的曲线
     for experiment in all_data['experiment'].unique():
         subset = all_data[all_data['experiment'] == experiment]
         # 计算90%的数据点位置
-        n = int(len(subset) * 1)
+        n_start = int(len(subset) * 0.2)
+        n_end = int(len(subset) * 0.8)
         # 截取0到90%的数据
-        subset_90 = subset.iloc[:n]
+        subset_80 = subset.iloc[n_start:n_end]
         # 绘制图表
         # 绘制real_voltage曲线（黑虚线，线宽2）
-        plt.plot(subset_90['real_time'], subset_90['real_voltage'],
+        plt.plot(subset['real_time'], subset['real_voltage'],
                  # label=f'Real Voltage {experiment}',
                  linestyle='--', linewidth=2, color='black')
         # 绘制simu_voltage曲线（实线，线宽2）
-        plt.plot(subset_90['real_time'], subset_90['simu_voltage'],
+        plt.plot(subset['real_time'], subset['simu_voltage'],
                  label=f'Simulation Voltage {experiment}',
                  linestyle='-', linewidth=2)  # 假设simu_voltage用蓝色表示
         # 计算RMSE
-        real_voltage = subset_90['real_voltage'].values
-        simu_voltage = subset_90['simu_voltage'].values
+        real_voltage = subset_80['real_voltage'].values
+        simu_voltage = subset_80['simu_voltage'].values
         mse = mean_squared_error(real_voltage, simu_voltage)
         rmse = np.sqrt(mse)
         rmse_values.append((experiment, rmse))
@@ -61,5 +62,6 @@ if __name__ == '__main__':
     for experiment, rmse in rmse_values:
         print(f'RMSE for {experiment}: {rmse:.4f}')
     # 显示图表
+    plt.tight_layout()
     plt.show()
 
